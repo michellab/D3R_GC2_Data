30da299268b3918fd7acbf50b15d0972edacdf46
ppxasjsm <antonia.mey@fu-berlin.de>
2017-07-24 14:38:01 +0100

    fesetup input stage2 d3rd2

---
+++ Input/D3RD2/stage2/fesetup/fesetup-FXR_79_BM2B.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -o FESetup-FXR_79_BM2B.out
+#SBATCH -p main
+#SBATCH --time 48:00:00
+#SBATCH -n 1
+#SBATCH -N 1
+#source /etc/profile.d/module.sh
+#module load FESetup
+
+FESetup setup-FXR_79_BM2B.in
+wait
